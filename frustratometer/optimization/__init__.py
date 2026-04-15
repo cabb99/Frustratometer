fe1d9501f4729afca8cac0d6b082eee4fdb7488d
Carlos Bueno <cabb99@gmail.com>
2026-04-14 19:11:23 -0500

    Lazy import in optimization to save 20s of numba compilation

--- frustratometer/optimization/__init__.py
+++ frustratometer/optimization/__init__.py
@@ -1,2 +1,15 @@
-from .optimization import *
-from .inner_product import build_mean_inner_product_matrix, permutation_numpy_inner_product
+def __getattr__(name):
+    import importlib
+    _opt = importlib.import_module(".optimization", __name__)
+    _ip = importlib.import_module(".inner_product", __name__)
+
+    _ns = {}
+    for _mod in (_opt, _ip):
+        for _k in dir(_mod):
+            if not _k.startswith("_"):
+                _ns[_k] = getattr(_mod, _k)
+    globals().update(_ns)
+
+    if name in _ns:
+        return _ns[name]
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
